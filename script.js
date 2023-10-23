29efafade85182f4055a5f2449a6f8d972414528
jemmaniduaza <2221083@slu.edu.ph>
2023-10-23 20:18:50 +0800

    initial nav bar

---
+++ script.js
@@ -0,0 +1,10 @@
+// index.html ("Search Destination Button")
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.querySelector(".custom-button");
+    button.addEventListener("click", function () {
+      window.location.href = "sample.html";
+    });
+  });
+
+  
+  
