f7f19db41c7d2068d29bb50d9399c1046e96ea47
Jemma Niduaza <2221083@slu.edu.ph>
2023-10-29 20:57:22 +0800

    internal js to external js for index.html

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,7 +1,20 @@
-// index.html ("Search Destination Button")
-document.addEventListener("DOMContentLoaded", function () {
-    var button = document.querySelector(".custom-button");
-    button.addEventListener("click", function () {
-      window.location.href = "destination.html";
-    });
-});
+// JavaScript for Image Slideshow
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  // Hide all images
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // Increment slideIndex and reset if it exceeds the number of images
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  // Display the current image
+  slides[slideIndex - 1].style.display = "block";
+  // Change image every 3 seconds (3000 milliseconds)
+  setTimeout(showSlides, 2000);
+}
