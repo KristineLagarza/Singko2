7f2f5eee732f864bbf2bd4c9b5c1269daa00a651
ODENEIL <2201701@slu.edu.ph>
2023-10-30 10:34:12 +0800

    fixed flight info display

--- flights-test.js
+++ flights-test.js
@@ -30,19 +30,20 @@ function submitUserInput() {
         let daysOfFlight = flightDaysToDayOfWeek(flightDetail.getAttribute('FLSFlightDays'));
         let tripInfo = convertTripInfo(flightDetail.getAttribute('TotalTripTime'), flightDetail.getAttribute('TotalFlightTime'), flightDetail.getAttribute('FLSDepartureDateTime'), flightDetail.getAttribute('FLSArrivalDateTime'));
         let kmInfo = milesToKilometers(flightDetail.getAttribute('TotalMiles'));
+        let departureInfo = extractDateAndTime(flightDetail.getAttribute('FLSDepartureDateTime'));
+        let arrivalInfo = extractDateAndTime(flightDetail.getAttribute('FLSArrivalDateTime'));
 
         const flight = {
           TotalFlightTime: tripInfo.flightDuration,
           TotalMiles: kmInfo.toFixed(2),
           TotalTripTime: tripInfo.tripDuration,
-          DepartureDateTime: tripInfo.departureTime,
+          DepartureDateTime: departureInfo.date + ' @ ' + tripInfo.departureTime,
           DepartureCode: flightDetail.getAttribute('FLSDepartureCode'),
           DepartureName: flightDetail.getAttribute('FLSDepartureName'),
-          ArrivalDateTime: tripInfo.arrivalTime,
+          ArrivalDateTime: arrivalInfo.date + ' @ ' + tripInfo.arrivalTime,
           ArrivalCode: flightDetail.getAttribute('FLSArrivalCode'),
           ArrivalName: flightDetail.getAttribute('FLSArrivalName'),
           FlightType: flightDetail.getAttribute('FLSFlightType'),
-          FlightLegs: flightDetail.getAttribute('FLSFlightLegs'),
           FlightDays: daysOfFlight,
         };
   
@@ -52,24 +53,22 @@ function submitUserInput() {
       });
   
       // Display flight information
-      const flightInfoDiv = document.getElementById('flightInfo');
-      flightInfoDiv.innerHTML = '<h2>Flight Information</h2>';
+      const flightInfoDiv = document.getElementById('flights-container');
       flightInfo.forEach((flight, index) => {
         flightInfoDiv.innerHTML += `
-          <div>
-            <h3>Flight ${index + 1}</h3>
-            <p>Total Flight Time: ${flight.TotalFlightTime}</p>
-            <p>Total Kilometers: ${flight.TotalMiles}</p>
-            <p>Total Trip Time: ${flight.TotalTripTime}</p>
+          <div class="flight-info">
+            <h2>Flight ${index + 1}</h2>
+            <h3>Departure</h3>
             <p>Departure Date/Time: ${flight.DepartureDateTime}</p>
-            <p>Departure Code: ${flight.DepartureCode}</p>
             <p>Departure Name: ${flight.DepartureName}</p>
+            <p>Departure Code: ${flight.DepartureCode}</p>
+            <h3>Arrival</h3>
             <p>Arrival Date/Time: ${flight.ArrivalDateTime}</p>
             <p>Arrival Code: ${flight.ArrivalCode}</p>
             <p>Arrival Name: ${flight.ArrivalName}</p>
+            <br><p>Total Flight Time: ${flight.TotalFlightTime}</p>
+            <p>Total Trip Time: ${flight.TotalTripTime}</p>
             <p>Flight Type: ${flight.FlightType}</p>
-            <p>Flight Legs: ${flight.FlightLegs}</p>
-            <p>Flight Days: ${flight.FlightDays}</p>
           </div>
         `;
       });
@@ -135,4 +134,22 @@ function submitUserInput() {
         const kilometers = miles * 1.60934;
         return kilometers;
     }
+
+    function extractDateAndTime(dateTimeString) {
+      const dateTimeRegex = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})$/;
+      const match = dateTimeString.match(dateTimeRegex);
+  
+      if (match) {
+          // Extract the matched date and time
+          const extractedDateTime = match[1];
+          const [date, time] = extractedDateTime.split('T');
+  
+          return {
+              date,
+              time
+          };
+      }
+  
+      return null; 
+  }
     
