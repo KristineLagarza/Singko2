dea133174b4cddae05111eb5c9f3ba2ed8de2c70
Jemma Niduaza <2221083@slu.edu.ph>
2023-11-01 03:33:55 +0800

    adeed back to top button

--- JavaScript/destination.js
+++ JavaScript/destination.js
@@ -20,3 +20,20 @@ document.getElementById('search-input').addEventListener('input', function () {
         }
     });
 });
+
+// JavaScript for Go to top button
+let mybutton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
